b43cc2dfd7fb15d3f6d67ee37ea8460abd16c827
Sri karan <166303720+DEMONALIZER@users.noreply.github.com>
2024-08-06 10:43:38 +0530

    Create Correct.sh

---
+++ Correct.sh
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#define MAX_FILES 10
+#define TOTAL_DISK_SPACE 1000
+
+int main() {
+    int file_sizes[MAX_FILES] = {100, 200, 150, 300}; // File sizes
+    int disk_space = 0; // Current allocated disk space
+
+    for (int i = 0; i < MAX_FILES; i++) {
+        if (disk_space + file_sizes[i] <= TOTAL_DISK_SPACE) { // Assuming total disk space is 1000
+            printf("File %d allocated at position %d\n", i + 1, disk_space);
+            disk_space += file_sizes[i]; // Increment disk space
+        } else {
+            printf("Disk space full. Cannot allocate File %d\n", i + 1);
+            break;
+        }
+    }
+
+    return 0;
+}
+
